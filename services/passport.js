d84c6e98b06bfe6e335e6e1ce33ca0609d97a517
probee <lebetlaf@gmail.com>
2017-08-06 00:41:51 +0000

    back to relative path for callback url

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "https://frozen-bayou-29017.herokuapp.com/auth/google/callback",
+      callbackURL: "/auth/google/callback",
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
