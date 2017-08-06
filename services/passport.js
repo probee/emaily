7d98d3698169f779f6e49a2774bce9420f10eb4f
probee <lebetlaf@gmail.com>
2017-08-06 00:37:27 +0000

    hard coding callback url

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://frozen-bayou-29017.herokuapp.com/auth/google/callback",
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
